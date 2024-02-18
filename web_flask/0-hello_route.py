c9dd29ec33d255d88ea684f7bf989532f88bc36f
hailegnaw27 <ixvos406@gmail.com>
2024-02-18 10:01:04 -0800

    1-hbnb_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ This module starts a Flask web application.
 from flask import Flask
 
 app = Flask(__name__)
-
+app.config["ENV"] = "development"
 
 @app.route('/', strict_slashes=False)
 def hello():
@@ -17,4 +17,4 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001, debug=True)
