0290a0bb9f2b6cfa57323a7f6a199725341ac8a3
hailegnaw27 <ixvos406@gmail.com>
2024-02-18 10:05:26 -0800

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
 """
-This module starts a Flask web application.
+This script starts a Flask web application.
 """
 
 from flask import Flask
 
 app = Flask(__name__)
-app.config["ENV"] = "development"
+
 
 @app.route('/', strict_slashes=False)
-def hello():
+def hello_hbnb():
     """
-    This function handles the root route and returns 'Hello HBNB!'
+    Displays "Hello HBNB!" when accessing the root route.
     """
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """
+    Displays "HBNB" when accessing the /hbnb route.
+    """
     return 'HBNB'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
