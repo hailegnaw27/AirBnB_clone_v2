99bbb6d654b736ab8225377404735f53d4486693
hailegnaw27 <ixvos406@gmail.com>
2024-02-18 10:22:13 -0800

    4-number_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Displays "Hello HBNB!" when accessing the root route.
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Displays "HBNB" when accessing the /hbnb route.
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Displays "C " followed by the value of the text variable.
+    Replace underscore _ symbols with a space.
+    """
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """
+    Displays "Python " followed by the value of the text variable.
+    Replace underscore _ symbols with a space.
+    """
+    return 'Python ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
